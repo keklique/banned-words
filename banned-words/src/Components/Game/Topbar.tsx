1f0d0b2b49ae1bcf5018d6eac36ef84a27b8c602
keklique <41877582+keklique@users.noreply.github.com>
2022-12-27 13:54:36 +0300

    topbar created

--- banned-words/src/Components/Game/Topbar.tsx
+++ banned-words/src/Components/Game/Topbar.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
-function Topbar() {
+function Topbar({turnOver}:{turnOver:()=>void}) {
+    const TEMP_TURN_DURATION:number = 30;
+    const [timeCounter,setTimeCounter] = useState(30);
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            if(timeCounter!=0)
+            {
+                setTimeCounter(timeCounter -1);
+            }else
+            {
+                turnOver();
+            }
+        },1000);
+        return ()=> clearInterval(interval);
+    },[timeCounter]);
   return (
-    <div>Topbar</div>
+    <div>{timeCounter}</div>
   )
 }
 
