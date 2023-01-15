bf564e2f9f90a87bab7d4f44752e1d848d34d6fe
keklique <41877582+keklique@users.noreply.github.com>
2023-01-16 02:32:35 +0300

    Vercel analitics added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Turn from './Components/Game/Turn';
 // import wordData from './words.json'
 import { IWordSet } from './types/game';
 import Game from './Components/Game/Game';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   // const data: Array<IWordSet> = JSON.parse(JSON.stringify(wordData));
@@ -13,6 +14,7 @@ function App() {
      <div className='font-overlock bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#871fb3]  to-[#5c079d] h-screen flex-1 max-w-full self-center sm:max-w-sm flex justify-center'>
        <Game/>
      </div>
+     <Analytics />
     </div>
   );
 }
